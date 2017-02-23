ecebbda738224ca46f1fb94f2cc3e1c904b9335b
Himanshu <himanshu@sahusoft.com>
2017-02-23 13:25:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import addImport from './addImport';
+import removeImport from './removeImport';
+import addEnv from './addEnv';
+import removeEnv from './removeEnv';
+import getRegistrationName from './getRegistrationName';
+import addUse from './addUse';
+import removeUse from './removeUse';
+import runGenerator from './runGenerator';
+
+export default {
+  addEnv: addEnv,
+  removeEnv: removeEnv,
+  addImport: addImport,
+  removeImport: removeImport,
+  addUse: addUse,
+  removeUse: removeUse,
+  getRegistrationName: getRegistrationName,
+  runGenerator: runGenerator,
+}
